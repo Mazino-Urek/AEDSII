ce8b11f4f7ea6f593e32fd3351545a17aa04345c
“Reinhard” <diego_ferreira021@outlook.com>
2020-09-19 23:02:25 -0300

    por segurança

---
+++ AEDSII-TP/tp02/TP02Q02 - Classe em C/classe.cpp
@@ -0,0 +1,189 @@
+/*
+ * Nome: Diego Da Silva Ferreira
+ * Matricula: 71 06 78
+ *
+ * V 1.1
+ *
+ * */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*difinindo o tipo Jogador*/
+typedef struct {
+
+	int id;
+	char *nome;
+	int altura;
+	int peso;
+	char *universidade;
+	int anoNascimento;
+	char *cidadeNascimento;
+	char *estadoNascimento;
+} Jogador;
+
+/*
+ * seta os valores de um jogador
+ * @param int id - valor do atributo id
+ * @param char *nome - valor do atributo nome
+ * @param int altura - valor do atributo altura
+ * @param int peso - valor do atributo peso
+ * @param char *universidade - valor do universidade
+ * @param int anoNascimento - valor do atributo anoNacimento
+ * @param char *cidadeNascimento - valor do atributo cidadeNascimetno
+ * @param char * estadoNascimneto - valor do atributo estadoNascimento
+ * @return Jogador resp - jogador com atributos setados
+ * */
+Jogador set (int id, char *nome, int altura, int peso,
+	     char *universidade , int anoNascimento,
+	     char *cidadeNascimento, char *estadoNascimento) {
+
+	Jogador resp;
+
+	resp.id = id;
+	resp.nome = nome;
+	resp.altura = altura;
+	resp.peso = peso;
+	resp.universidade = universidade;
+	resp.anoNascimento = anoNascimento;
+	resp.cidadeNascimento = cidadeNascimento;
+	resp.estadoNascimento = estadoNascimento;
+
+	return resp;
+}//end set
+
+
+/*
+ * le os atributos de Jogador de um arquivo .csv 
+ * @param char* fileName - nome do arquivo a ser lido
+ * @return Jogador resp - array de jogaorres com artributos lidos do arquivo
+ * */
+Jogador *ler (char *fileName){
+
+	char string[150];
+	Jogador *resp;
+	string[149] = '\0';
+	char *aux1 , *aux2 , *aux3;
+	int n = 0;
+
+	
+	FILE* arq = fopen(fileName, "r");
+
+	/*metodo da saber o tamnho do arquivo*/
+	while(fgets(string, 148 , arq) != NULL)
+		n++;
+
+	resp =(Jogador *) calloc (n-1 , sizeof(Jogador));
+
+	fseek(arq, 0 , SEEK_SET);
+	fgets(string, 148, arq);
+
+	/*medodo de leitura do aquivo*/
+	for(int i = 0; i < n - 1; i++ ){
+	
+		fgets(string, 148, arq);
+		string[strlen(string) - 1] = ',';
+		aux1 = strdup(string);
+
+		aux2 = strsep(&aux1, ",");
+
+		if(strlen(aux2) != 0)
+			resp[i].id = atoi(aux2);
+		else
+			resp[i].id = -1;
+	
+		aux2 = strsep(&aux1, ",");
+
+		if(strlen(aux2) != 0)
+			resp[i].nome = aux2;
+		else
+			resp[i].nome = strdup("nao informado");
+	
+
+		aux2 = strsep(&aux1 , ",");
+
+		if(strlen(aux2) != 0)
+			resp[i].altura = atoi(aux2);
+		else
+			resp[i].altura = -1;
+
+		aux2 = strsep(&aux1 , ",");
+
+		if(strlen(aux2) != 0)
+			resp[i].peso = atoi(aux2);
+		else
+			resp[i].peso = -1;
+	
+		
+		aux2 = strsep(&aux1 , ",");
+
+		if(strlen(aux2) != 0)
+			resp[i].universidade = aux2;
+		else
+			resp[i].universidade = strdup("nao informado");
+	
+		aux2 = strsep(&aux1 , ",");
+
+		if(strlen(aux2) != 0)
+			resp[i].anoNascimento = atoi(aux2);
+		else
+			resp[i].anoNascimento = -1;
+	
+		aux2 = strsep(&aux1 , ",");
+
+		if(strlen(aux2) != 0)
+			resp[i].cidadeNascimento = aux2;
+		else
+			resp[i].cidadeNascimento = strdup("nao informado");
+	
+		aux2 = strsep(&aux1 , ",");
+
+		if(strlen(aux2) != 0)
+			resp[i].estadoNascimento = aux2;
+		else
+			resp[i].estadoNascimento = strdup("nao informado");
+				
+	}//end for
+
+	fclose(arq);
+	
+	return resp;
+}//end ler
+
+/*
+ * printa os atributos de um Jogador
+ * @param Jogador in - jogador a ter seus atributos printados
+ * */
+void imprimir (Jogador in) {
+
+	printf("[%d ## %s ## %d ## %d ## %d ## %s ## %s ## %s]\n" , 
+		in.id,
+		in.nome,
+		in.altura,
+		in.peso,
+		in.anoNascimento,
+		in.universidade,
+		in.cidadeNascimento,
+		in.estadoNascimento);
+}//end imprimir
+
+
+int main (void){
+
+	Jogador *jogador = ler(strdup("/tmp/players.csv"));
+	char novo[10];
+
+	scanf("%s" , novo);
+
+	while(strcmp(novo , "FIM") != 0){
+	
+		imprimir(jogador[atoi(novo)]);
+
+		scanf("%s" , novo);	
+	}
+
+	free(jogador);
+
+	return 0;
+}//end main
